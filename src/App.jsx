5441950ba8de3dc8e213324df78d8c021a056ee2
Rodrigo Strapazzon <114418794+RodrigoStrapazzon@users.noreply.github.com>
2022-11-17 19:41:25 -0300

    v2
    date and post  format

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,37 @@ import styles from "./App.module.css";
 
 import './global.css';
 
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://github.com/RodrigoStrapazzon.png',
+      name: 'Rodrigo Strapazzon',
+      role: 'Developer @Nebula',
+    },
+    content: [
+      {type: 'paragraph', content: 'Fala galeraa'},
+      {type: 'paragraph', content: '👋 Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀'},
+      {type: 'link', content: 'jane.design/doctorcare'},
+    ],
+    publishedAt: new Date('2022-11-11 18:08:00'),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://github.com/pedro-lucinda.png',
+      name: 'Pedro Lucinda',
+      role: 'CEO @Nebula',
+    },
+    content: [
+      {type: 'paragraph', content: 'Fala galeraa'},
+      {type: 'paragraph', content: '👋 Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀'},
+      {type: 'link', content: 'jane.design/doctorcare'},
+    ],
+    publishedAt: new Date('2022-11-13 18:08:00'),
+  },
+];
+
 export default function App() {
   return (
     <div>
@@ -14,8 +45,15 @@ export default function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <Post />
-          <Post />
+          {posts.map(posts => {
+            return (
+              <Post 
+                author={posts.author}
+                content={posts.content}
+                publishedAt={posts.publishedAt}
+              />
+            )
+          })}
         </main>
       </div>
     </div>
